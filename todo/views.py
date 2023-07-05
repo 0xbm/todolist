227afab39271d067dbce3dfcc29b4c0ffa00aef2
0xbm <0xmb@protonmail.com>
2023-07-05 20:28:48 +0200

    add new views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
-# Create your views here.
+
+# def index(request):
+#     return HttpResponse("index.html")
+
+
+def index(request):
+    return render(request, "todo/index.html")
+    # return render(request, "task_list/task_list.html")
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todo/task_detail.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
