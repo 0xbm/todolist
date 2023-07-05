7260d5b14f890e056b6dbe08934dbc4b6960e93a
0xbm <0xmb@protonmail.com>
2023-07-05 20:28:30 +0200

    add urls

---
+++ todo/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate,DeleteView
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # path("task/", views.taskList, name="tasks"),
+    path("task_list/", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("task_create/", TaskCreate.as_view(), name="task_create"),
+    path("task_update/<int:pk>/", TaskUpdate.as_view(), name="task_update"),
+    path("task_delete/<int:pk>/", DeleteView.as_view(), name="task_delete"),
+
+]
